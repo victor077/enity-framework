90b6806b7ac55ffaee3f3e1898af13db26a43278
victor077 <victor.souza@bandtec.com.br>
2023-02-21 23:26:04 -0300

    configuração dos atributos e funcionalidades que serão criados no meu banco de dados atraves do mue contenxt de banco

--- CursoEFCore/Domain/Produto.cs
+++ CursoEFCore/Domain/Produto.cs
@@ -10,6 +10,7 @@ namespace CursoEFCore.Domain
     {
         public int Id { get; set; }
         public string? CodigoBarras { get; set; }
+        public string? Descricao { get; set; }
         public decimal Valor { get; set; }
         public TipoProduto TipoProduto { get; set; }
         public bool Ativo { get; set; }
